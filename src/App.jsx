3ef9f6a0f6a75bb0ae0e48c7f28ea1215b70ac61
Manoj <manoj.gupta16539@sakec.ac.in>
2024-06-07 17:12:36 +0530

    chore: Refactor App.jsx to fetch and display products by category

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 function App() {
     let[finalCategory,setFinalCategory]=useState([])
+    let[finalProduct, setFinalProduct]=useState([])
     let getCategory=()=>{
         axios.get('https://dummyjson.com/products/category-list')
         .then((res) =>res.data)
@@ -13,16 +14,23 @@ function App() {
         })
     }
     let getProducts=()=>{
-        axios.get('https://dummyjson.com/products')
+        axios.get('https://dummyjson.com/products/category/smartphones')
         .then((proRes) =>proRes.data)
         .then((finalRes) => {
-            console.log(finalRes);
+            setFinalProduct(finalRes.products);
         })
     }
     useEffect(()=>{
         getCategory();
         getProducts();
     },[])
+
+    let Pitems=finalProduct.map((products, index)=>{
+        return(
+        <ProductItems key={index} pdata={products} />
+        ) 
+    })
+
   return (
     <>
       <div className="py-[40px]">
@@ -37,10 +45,8 @@ function App() {
               <Category  finalCategory={finalCategory} />
             </div>
             <div>
-              <div className="grid grid-cols-3 gap-4">
-                <ProductItems /> 
-                <ProductItems /> 
-                <ProductItems /> 
+              <div className="grid grid-cols-3 gap-5">
+                {finalProduct.length>=1 ? Pitems : 'No Products Found'}
               </div>
             </div>
           </div>
@@ -52,13 +58,16 @@ function App() {
 
 export default App;
 
-function ProductItems() {
+function ProductItems(pdata) {
+    console.log(pdata)
     return(
         <div className="shadow-lg  text-center pb-4">
                   <img
-                    src="https://yuvaanjewels.com/wp-content/uploads/2023/10/IMG_20231031_142404_214.jpg"alt="Astrid Polki Bracelet"/>
-                  <h4>IPHONE</h4>
-                  <b>₹ 1,00,000</b>
+                    src={pdata.pdata.thumbnail} className='w-[100%] h-[220px]'>
+
+                    </img>
+                    <h4>{pdata.pdata.title}</h4>
+                  <b>{pdata.pdata.price}</b>
         </div>
     )
 }
